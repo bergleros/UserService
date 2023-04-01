e6bf4dada7da5514af9d1eb418ebd3d4d670fd37
bergleros <101412205+bergleros@users.noreply.github.com>
2023-04-01 23:19:25 +0000

    Clarify comments

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -20,11 +20,12 @@ namespace UserService.Controllers
         /// <summary>
         /// Get userid from a provided secret. If no user exists for the secret, it will be created and the new userid returned.
         ///
-        /// If the provided input is actually meant to be a secret that allows the user to get their information then obviously this solution is extremely vulnerable to dictionary attacks,
-        /// especially with the minimum length being only 3 characters. It must also be considered quite likely that two users will provide the same secret and subsequently get the same userid.
-        /// It is not clear from the description what the intention of the method is, and in a real-world project I would request clarification to better understand what the requirements are
-        /// in order to improve the implementation. Some ideas that come to mind include increasing the minimum length, have the user provide another piece of data such as username or a 3rd party auth token,
-        /// or generate a key for the user to sign subsequent requests.
+        /// If the provided input is actually meant to be a secret that allows the user to get their information, then obviously this solution is extremely vulnerable to
+        /// dictionary attacks, especially with the minimum length being only 3 characters. It must also be considered quite likely that two users will provide the same 
+        /// secret and subsequently get the same userid.
+        /// It is not clear from the description what the intention of the method is, and in a real-world project I would request clarification to better understand what
+        /// the requirements are in order to improve the implementation. Some ideas that come to mind include increasing the minimum length, have the user provide another 
+        /// piece of data such as username or a 3rd party auth token, or generate a key for the user to sign subsequent requests.
         /// </summary>
         /// <param name="request"></param>
         /// <returns>Userid for the provided secret</returns>
